3796683c9ef3ef5904ba39b45cc4fbe3571dd5ca
Alex Zhang <alexgzhang92@gmail.com>
2018-05-30 22:41:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,6 +12,7 @@ class Song
   attr_accessor :name, :artist
 
   def self.new_by_filename(filename)
+    binding.pry
     song_name = filename.split(" - ")[1]
     name_of_artist = filename.split(" - ")[0]
     song = Song.new(song_name)
